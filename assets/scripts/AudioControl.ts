6862a1d777505ecf8db8db810893797e7fd11446
Mujiang <formateric@gmail.com>
2022-09-17 21:31:13 +0800

    init

---
+++ assets/scripts/AudioControl.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('AudioControl')
+export class AudioControl extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
