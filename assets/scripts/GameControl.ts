99490b6de2ca5c33e905ab84c30ee2e34f445792
Mujiang <formateric@gmail.com>
2022-10-02 11:38:56 +0800

    1.4.2

--- assets/scripts/GameControl.ts
+++ assets/scripts/GameControl.ts
@@ -1,12 +1,18 @@
 import {
     _decorator, Component, director,
-    find, Node, Animation, resources
+    find, Node, Animation, resources, game, assetManager,
+    Sprite, SpriteFrame, ImageAsset, Texture2D, AudioSource, AudioClip
 } from 'cc';
 import WXAPI from "db://assets/scripts/WXAPI";
+import {clickAudio} from "db://assets/scripts/clickAudio";
 const { ccclass, property } = _decorator;
+
+let firstLoad = true;
 
 @ccclass('GameControl')
 export class GameControl extends Component {
+    private audio:AudioSource = new AudioSource();
+    time = Date.now()
     start() {
         if (director.getScene().name == "game") {
             // 找不到节点
@@ -15,18 +21,47 @@ export class GameControl extends Component {
             // console.log('fadeOut')
             // trans.play("fadeOut");
         }
+        if (director.getScene().name == "home") {
+            assetManager.loadRemote("https://prod-8g71dxke43f8814e-1314041846.tcloudbaseapp.com/uploads/gongzhonghao.png", (err, asset:ImageAsset)=>{
+                let popup = find('gongzhonghao/gongzhonghao');
+                const spriteFrame = new SpriteFrame();
+                const texture = new Texture2D();
+                texture.image = asset;
+                spriteFrame.texture = texture
+                popup.getComponent(Sprite).spriteFrame = spriteFrame;
+            })
+        }
+
+        if (firstLoad) {
+            firstLoad = false;
+            WXAPI.login();
+
+            // resources.preloadDir('icons', console.log)
+            // resources.preloadDir('home', console.log)
+            // resources.preloadDir('gaming-juan', console.log)
+            // resources.preloadDir('audio', console.log)
+            director.preloadScene("game", () => {
+                resources.preloadDir('icons')
+            })
+        }
+        console.log('start', Date.now() - this.time)
     }
 
     update(deltaTime: number) {
     }
 
     onLoad() {
-        WXAPI.login();
-        resources.preloadDir('icons')
-        resources.preloadDir('home')
-        resources.preloadDir('gaming-juan')
-        resources.preloadDir('audio')
+        console.log('onLoad', Date.now() - this.time)
         // console.log('game control loaded')
+        wx.onShareAppMessage( () => {
+            return {
+                // 标题，不传则默认使用小游戏的名称
+                title: "邀您一起来嗨！",
+
+                // 转发链接所显示的图片，比例5:4，资源可以是本地或远程。不传则默认使用游戏截图。
+                imageUrl: "shareImage.png"
+            }
+        });
     }
 
     startGame() {
@@ -39,6 +74,10 @@ export class GameControl extends Component {
         }, 0.3)
         console.log('start');
         trans.play();
+        if ( ! clickAudio.isEnabled()) return;
+        resources.load("audio/trans", AudioClip, (err, audio)=>{
+            this.audio.playOneShot(audio);
+        })
     }
 
     backToHome() {
@@ -49,7 +88,12 @@ export class GameControl extends Component {
             director.loadScene("home");
         }, 0.3)
         trans.play();
+        if ( ! clickAudio.isEnabled()) return;
+        resources.load("audio/trans", AudioClip, (err, audio)=>{
+            this.audio.playOneShot(audio);
+        })
     }
+
 }
 
 
