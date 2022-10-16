fd6ceec2cde1ce2092e9957b2639cf1c9fac2714
Mujiang <formateric@gmail.com>
2022-10-16 18:29:13 +0800

    排行榜结构调整，还缺图

--- assets/scripts/RankListDataControl.ts
+++ assets/scripts/RankListDataControl.ts
@@ -18,35 +18,22 @@ export class RankListDataControl extends Component {
     private Otherman: Prefab = null;
     async start() {
         let res = await this.getRankData()
+        let parent = find("RankListContainer/RankListContainer/ScrollView/view/content")
         let dataArr = []
+
         for (let i = 0; i < res.length; i++) {
             dataArr.push({ nickname: res[i].name, minutes: Math.trunc(res[i].time / 60), seconds: res[i].time % 60 })
         }
-        /*for (let i = 0; i < 30; i++) {
-            dataArr.push({ nickname: "开岸", minutes: "12", seconds: "45" })
-        }*/
+        // for (let i = 0; i < 30; i++) {
+        //     dataArr.push({ nickname: "开岸", minutes: "12", seconds: "45" })
+        // }
+
         for (let i = 0; i < dataArr.length; i++) {
             let man = null
-            if (i == 0) {
-                man = instantiate(this.firstman)
-            } else if (i == 1) {
-                man = instantiate(this.secondman)
-            } else if (i == 2) {
-                man = instantiate(this.thirdman)
-            } else {
-                man = instantiate(this.Otherman)
-            }
-
-
-            let parent = find("RankListContainer/RankListContainer/ScrollView/view/content")
-            if (i < 3) {
-                man.setPosition(70, -740 - 140 * (i))
-                this.setTopThreeMan(man, dataArr[i])
-            } else {
-                man.setPosition(8, -780 - 140 * (i))
-                this.setOtherManLebal(man, i, dataArr[i])
-            }
-            man.parent = parent
+            man = instantiate(this.Otherman)
+            console.log(man, i, dataArr[i])
+            this.setOtherManLebal(man, i, dataArr[i])
+            parent.addChild(man)
         }
     }
 
@@ -61,26 +48,14 @@ export class RankListDataControl extends Component {
             numLabel.string = (index + 1)
         }
         //设置nickname
-        let nickname = node.getChildByName('thirdMan').getChildByName('nickname')
+        let nickname = find('right/nickname', node)
         let nicknameLabel = nickname.getComponents(Label)[0]
         nicknameLabel.string = data.nickname || ''
         //设置时和分
-        let timer = node.getChildByName('thirdMan').getChildByName('time')
-        let minLabel = timer.getChildByName('minutes').getComponents(Label)[0]
+        let minLabel = find('right/time/minutes', node).getComponents(Label)[0]
         minLabel.string = data.minutes
-        let secLabel = timer.getChildByName('seconds').getComponents(Label)[0]
-        secLabel.string = data.seconds
-    }
-    setTopThreeMan(node, data) {
-        //设置nickname
-        let nickname = node.getChildByName('nickname')
-        let nicknameLabel = nickname.getComponents(Label)[0]
-        nicknameLabel.string = data.nickname || ''
-        //设置时和分
-        let timer = node.getChildByName('time')
-        let minLabel = timer.getChildByName('minutes').getComponents(Label)[0]
-        minLabel.string = data.minutes
-        let secLabel = timer.getChildByName('seconds').getComponents(Label)[0]
+
+        let secLabel = find('right/time/seconds', node).getComponents(Label)[0]
         secLabel.string = data.seconds
     }
     //查询排行榜数据
