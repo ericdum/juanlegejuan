09fd769089dc19bd54597ef3acbd5438748ad195
Mujiang <formateric@gmail.com>
2022-10-16 14:40:44 +0800

    1、把用户信息的button从全屏改为排行榜 2、完成排行榜相关API

--- assets/scripts/WXAPI.ts
+++ assets/scripts/WXAPI.ts
@@ -1,4 +1,5 @@
-import { sys } from "cc";
+import {sys, find, UITransform, Vec3, Node} from "cc";
+declare var wx: any;
 
 class WXAPI {
     cloudInited = false
@@ -12,6 +13,7 @@ class WXAPI {
         if (typeof wx == 'undefined') return;
         let envdata = this.getEnv()
 
+        // develop, trial, release
         if (envdata?.miniProgram?.envVersion === 'develop') {
             //当前为测试环境，需要去修改env
             this.env = 'pre-3g8yq30806c4fdf0'
@@ -31,6 +33,7 @@ class WXAPI {
             return this.shareOption
         })
     }
+
     private refresh() {
         this.call({
             path: "/share"
@@ -38,6 +41,7 @@ class WXAPI {
             this.shareOption = data;
         })
     }
+
     private async init() {
         if (typeof wx == 'undefined') return;
         if (!this.cloudInited) {
@@ -49,7 +53,39 @@ class WXAPI {
             await wx.cloud.init() // init过程是异步的，需要等待 init 完成才可以发起调用
             this.cloudInited = true;
         }
+        // if (director.getScene().name == "home") {
+        //     // 找不到节点
+        //     // const ly:Node = find('Canvas/TransitionMaskLayout');
+        //     // const trans = ly.getComponent(Animation);
+        //     // console.log('fadeOut')
+        //     // trans.play("fadeOut");
+        //
+        //     const updateManager = wx.getUpdateManager()
+        //
+        //     updateManager.onCheckForUpdate(function (res) {
+        //         // 请求完新版本信息的回调
+        //         console.log(res.hasUpdate)
+        //     })
+        //
+        //     updateManager.onUpdateReady(function () {
+        //         wx.showModal({
+        //             title: '更新提示',
+        //             content: '新版本已经准备好，是否重启应用？',
+        //             success(res) {
+        //                 if (res.confirm) {
+        //                     // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        //                     updateManager.applyUpdate()
+        //                 }
+        //             }
+        //         })
+        //     })
+        //
+        //     updateManager.onUpdateFailed(function () {
+        //         // 新版本下载失败
+        //     })
+        // }
     }
+
     public login() {
         if (typeof wx == 'undefined') return;
         this.call({
@@ -108,7 +144,7 @@ class WXAPI {
         })
     }
 
-    public video(msg, resolve) {
+    public video(msg, resolve, reject=()=>{}) {
         if (typeof wx == 'undefined') return resolve();
         wx.showLoading({
             title: '加载中',
@@ -153,7 +189,7 @@ class WXAPI {
         })
     }
 
-    public async call(obj: object, number = 0) {
+    public async call(obj, number = 0) {
         if (typeof wx == 'undefined') return;
         await this.init();
         const that = this;
@@ -194,12 +230,10 @@ class WXAPI {
         if (typeof wx == "undefined") return;
         wx.reportEvent(id, data)
     }
-    public userlogin(): void {
+    public getUserInfoOverButton(node): void {
         const sdk = this
         if (typeof wx == 'undefined') return;
         let sysInfo = wx.getSystemInfoSync();
-        let width = sysInfo.screenWidth;
-        let height = sysInfo.screenHeight;
         wx.getSetting({
             success(res) {
                 console.log("res.authSetting:" + res.authSetting);
@@ -208,22 +242,29 @@ class WXAPI {
                     wx.getUserInfo({
                         success(res) {
                             console.log(JSON.stringify(res));
-                            sdk.meoData(res.userInfo)
+                            sdk.updateUserInfo(res.userInfo)
                         }
                     });
                 }
                 else {
+                    let uit:UITransform = node.getComponent(UITransform)
+                    let pos:Vec3 = node.getWorldPosition()
+                    let canvas:UITransform = find('Canvas').getComponent(UITransform)
+                    let dx = sysInfo.screenWidth / canvas.width;
+                    let dy = sysInfo.screenHeight / canvas.height;
+
                     console.log("用户未授权");
                     //用户未授权的话，全屏覆盖一个按钮，用户点击任意地方都会触发onTap()，弹出授权界面
                     let button = wx.createUserInfoButton({
                         type: 'text',
                         text: '',//不显示文字
                         style: {
-                            left: 0,
-                            top: 0,
-                            width: width,
-                            height: height,
+                            left: (pos.x - uit.width/2)*dx,
+                            top: (canvas.height-pos.y-uit.height/2)*dy,
+                            width: uit.width*dx,
+                            height: uit.height*dy,
                             // lineHeight: 40,
+                            // backgroundColor: '#111111',//设置按钮透明
                             backgroundColor: '#00000000',//设置按钮透明
                             color: '#ffffff',
                             textAlign: 'center',
@@ -235,7 +276,7 @@ class WXAPI {
                         (res) => {
                             if (res.userInfo) {
                                 console.log("用户授权：" + JSON.stringify(res));
-                                sdk.meoData(res.userInfo)
+                                sdk.updateUserInfo(res.userInfo)
                                 //TODO：others
                                 button.destroy();//此时删除按钮
 
@@ -245,17 +286,27 @@ class WXAPI {
                                 console.log("用户拒绝授权");
                                 button.destroy();
                             }
+                            node.emit(Node.EventType.TOUCH_START)
+                            node.emit(Node.EventType.TOUCH_END)
                         }
                     );
                 }
             }
         });
     }
-    public meoData(data) {
+    public updateUserInfo(data) {
         //存储头像地址
         sys.localStorage.setItem("nickName", data?.nickName);
         //存储昵称
         sys.localStorage.setItem("avatarUrl", data?.avatarUrl);
+        this.call({
+            path: "/user/info",
+            method: 'POST',
+            data: {
+                nickname: data.nickName,
+                avatar: data.avatarUrl
+            }
+        })
     }
     public getEnv() {
         return wx.getAccountInfoSync()
